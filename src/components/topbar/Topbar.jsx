a901976afc79e0b9806c2fe424dbcf207bf6a9e2
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-01-07 08:42:43 +0600

    Create Topbar.jsx

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+export const Topbar = () => {
+  return (
+    <div className="top-bar">
+      <div className="logo">
+        <i className="fas fa-dice-six"></i>
+      </div>
+      <div className="nav-bar">
+        <ul>
+          <li className="active">home</li>
+          <li>about</li>
+          <li>contact</li>
+          <li>help</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
