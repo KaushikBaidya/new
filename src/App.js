359def050f351c446d15511be53bc6b854f3c9e0
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-01-07 13:11:06 +0600

    intro page completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Topbar } from './components/topbar/Topbar'
+import { Intro } from './components/intro/Intro'
 
 function App() {
   return (
     <div className="App">
-      <Topbar />
+      <Intro />
     </div>
   )
 }
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,28 @@
+import { Topbar } from '../topbar/Topbar'
+import './intro.css'
+
+export const Intro = () => {
+  return (
+    <div className="container">
+      <Topbar />
+      <div className="content">
+        <div className="intro">Hi, I'm Kaushik Baidya</div>
+        <div className="title">
+          I am a <span style={{ color: 'tomato' }}>Web Developer</span>
+        </div>
+        <div className="desc">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis,
+          atque sit dolore quia placeat adipisci repudiandae maxime deserunt.
+          Error, itaque possimus dicta mollitia dolores modi sequi fuga ipsam.
+          Saepe modi nostrum dolores ullam quo quasi voluptatum blanditiis
+          distinctio? Mollitia sit deleniti dolorum? Quas perspiciatis voluptate
+          asperiores labore? Corporis, suscipit! Sapiente!
+        </div>
+        <button className="btn">
+          {' '}
+          <span>Contact ME</span>{' '}
+        </button>
+      </div>
+    </div>
+  )
+}
